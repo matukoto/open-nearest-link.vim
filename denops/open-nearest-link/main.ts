1f1234014ddbc7ea4ba395019f40b5fe6e7ab84a
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2025-01-18 18:06:46 +0900

    feat(): popup window が開いて消えるようにした

--- denops/open-nearest-link/main.ts
+++ denops/open-nearest-link/main.ts
@@ -9,37 +9,27 @@ export const main: Entrypoint = (denops) => {
     fetch: async (url) => {
       assert(url, is.String);
       const text = await postchanFetch(url);
-      console.log(text);
 
       // Create a new buffer
-      const bufinfo = await buffer.open(denops, 'hello');
-      const { bufnr } = bufinfo;
-      // const bufnr = await fn.bufadd(denops, '');
+      const bufnr = await fn.bufadd(denops, '');
       await fn.bufload(denops, bufnr);
-
       // Write some text to the buffer
       await buffer.replace(denops, bufnr, text.split('\n'));
-      //
-      // // Open a popup window showing the buffer
-      // const popupWindow = await popup.open(denops, {
-      //   bufnr,
-      //   relative: 'editor',
-      //   width: 200,
-      //   height: 200,
-      //   row: 10,
-      //   col: 10,
-      // });
-      //
-      // // Config a popup window
-      // await popup.config(denops, popupWindow.winid, {
-      //   title: 'Hello, world!',
-      // });
-      //
-      // // Wait 3 seconds
+
+      // Open a popup window showing the buffer
+      await using _popupWindow = await popup.open(denops, {
+        bufnr,
+        relative: 'editor',
+        width: 200,
+        height: 20,
+        row: 10,
+        col: 3,
+      });
+
+      // Wait 3 seconds
+      // redrawしないとpopupWindow が描画されない
+      await denops.cmd('redraw');
       await new Promise((resolve) => setTimeout(resolve, 3000));
-      //
-      // // Close the popup window
-      // await popupWindow.close();
     },
   };
 };
