3ba9867bb245709269b6c718542ace83f1f440d6
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2025-01-18 18:00:36 +0900

    feat(popup): add popup std

--- denops/open-nearest-link/main.ts
+++ denops/open-nearest-link/main.ts
@@ -1,5 +1,8 @@
 import type { Entrypoint } from 'jsr:@denops/std';
 import { assert, is } from 'jsr:@core/unknownutil';
+import * as buffer from 'jsr:@denops/std/buffer';
+import * as fn from 'jsr:@denops/std/function';
+import * as popup from 'jsr:@denops/std/popup';
 
 export const main: Entrypoint = (denops) => {
   denops.dispatcher = {
@@ -7,6 +10,36 @@ export const main: Entrypoint = (denops) => {
       assert(url, is.String);
       const text = await postchanFetch(url);
       console.log(text);
+
+      // Create a new buffer
+      const bufinfo = await buffer.open(denops, 'hello');
+      const { bufnr } = bufinfo;
+      // const bufnr = await fn.bufadd(denops, '');
+      await fn.bufload(denops, bufnr);
+
+      // Write some text to the buffer
+      await buffer.replace(denops, bufnr, text.split('\n'));
+      //
+      // // Open a popup window showing the buffer
+      // const popupWindow = await popup.open(denops, {
+      //   bufnr,
+      //   relative: 'editor',
+      //   width: 200,
+      //   height: 200,
+      //   row: 10,
+      //   col: 10,
+      // });
+      //
+      // // Config a popup window
+      // await popup.config(denops, popupWindow.winid, {
+      //   title: 'Hello, world!',
+      // });
+      //
+      // // Wait 3 seconds
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      //
+      // // Close the popup window
+      // await popupWindow.close();
     },
   };
 };
