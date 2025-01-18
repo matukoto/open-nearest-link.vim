168d556d5ec2f058a24230aa0c8f1835151eb7fb
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2025-01-18 17:40:00 +0900

    feat(denops): view response text in console

--- denops/open-nearest-link/main.ts
+++ denops/open-nearest-link/main.ts
@@ -1,10 +1,18 @@
-import type { Entrypoint } from 'jsr:@denops/std@7.4.0';
-import { assert, is } from 'jsr:@core/unknownutil@3.18.1';
+import type { Entrypoint } from 'jsr:@denops/std';
+import { assert, is } from 'jsr:@core/unknownutil';
 
 export const main: Entrypoint = (denops) => {
   denops.dispatcher = {
-    fetch: (url) => {
-      console.log(url);
+    fetch: async (url) => {
+      assert(url, is.String);
+      const text = await postchanFetch(url);
+      console.log(text);
     },
   };
 };
+
+async function postchanFetch(url: string): Promise<string> {
+  const response = await fetch(url);
+  const text = await response.text();
+  return text;
+}
